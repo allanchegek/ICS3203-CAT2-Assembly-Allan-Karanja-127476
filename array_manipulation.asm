56ff9ba5c3a547650927fd24bbaa64bbebd2fddb
Allan Chege <68268573+allanchegek@users.noreply.github.com>
2024-11-27 16:24:18 +0300

    Update array_manipulation.asm

--- array_manipulation.asm
+++ array_manipulation.asm
@@ -1,29 +1,57 @@
 section .data
-    prompt db "Enter 5 integers separated by spaces: ", 0
-    result db "Reversed array: ", 0
+    prompt db "Enter a number: ", 0
+    pos_msg db "POSITIVE", 0
+    neg_msg db "NEGATIVE", 0
+    zero_msg db "ZERO", 0
 
 section .bss
-    array resb 5      ; Array to store 5 integers
+    num resb 4
 
 section .text
     global _start
 
 _start:
-    ; Read input
-    mov rax, 0        ; sys_read
-    lea rsi, [prompt]
+    ; Prompt the user
+    mov rax, 1           ; syscall: write
+    mov rdi, 1           ; file descriptor: stdout
+    mov rsi, prompt      ; message address
+    mov rdx, 14          ; message length
     syscall
 
-    ; Reverse the array in place
-    mov rdi, 0        ; Start index
-    mov rsi, 4        ; End index
-reverse_loop:
-    cmp rdi, rsi
-    jge reverse_done
-    ; Swap array[rdi] and array[rsi]
-    ; Increment rdi and decrement rsi
-    jmp reverse_loop
+    ; Read input
+    mov rax, 0           ; syscall: read
+    mov rdi, 0           ; file descriptor: stdin
+    mov rsi, num         ; buffer address
+    mov rdx, 4           ; buffer size
+    syscall
 
-reverse_done:
-    lea rsi, [result]
+    ; Convert input to integer
+    mov rax, [num]       ; load input into rax
+    sub rax, 48          ; convert ASCII to integer
+
+    ; Conditional logic
+    cmp rax, 0
+    je zero              ; jump if zero
+    jl negative          ; jump if less than zero
+
+positive:
+    mov rsi, pos_msg
+    jmp print_message
+
+negative:
+    mov rsi, neg_msg
+    jmp print_message
+
+zero:
+    mov rsi, zero_msg
+
+print_message:
+    mov rax, 1
+    mov rdi, 1
+    mov rdx, 8           ; length of message
+    syscall
+
+    ; Exit program
+    mov rax, 60          ; syscall: exit
+    xor rdi, rdi         ; return code: 0
     syscall
